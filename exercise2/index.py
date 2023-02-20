0cc1d063a5103937a3c6144b59e985e6f5396324
ImmortalSnail <teddybrosevelt8@gmail.com>
2023-02-19 21:34:12 -0500

    exercise2 complete

--- exercise2/index.py
+++ exercise2/index.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 
 
 app = Flask(__name__)
@@ -6,3 +7,17 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def exercise2():
     return render_template("index.html")
+
+@app.route("/calculate", methods=['GET', 'POST'])
+def calc():
+    good_input = True
+    num = request.form.get("number")
+    if num.isdigit() == False and num != '':
+        good_input = False
+        return render_template("calculate.html", number=0, inp=good_input)
+    elif num.isdigit() == False:
+        good_input = False
+        return render_template("calculate.html", number=1, inp=good_input)
+    else:
+        num = int(request.form.get("number"))
+        return render_template("calculate.html", number=num, inp=good_input)
