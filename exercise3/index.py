a33d8a0dfc44d13da6749a1024bf200bc63b3f27
ImmortalSnail <teddybrosevelt8@gmail.com>
2023-02-19 22:25:52 -0500

    exercise 3 done

--- exercise3/index.py
+++ exercise3/index.py
@@ -3,11 +3,16 @@ from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
+dict = {}
 
 @app.route("/", methods=['GET', 'POST'])
 def exercise3():
     return render_template("index.html")
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/registration", methods=['GET', 'POST'])
 def register():
-    return render_template("index.html")
+    name = request.form.get("name")
+    club = request.form.get("club")
+    dict[name] = club
+
+    return render_template("registration.html", dict=dict)
